09a94be2174375d48c2cc4c38f8d87b24bae93fe
moonvolk4 <84585564+moonvolk4@users.noreply.github.com>
2024-11-11 13:53:56 +0300

    Create main.py

---
+++ labs/lab1/main.py
@@ -0,0 +1,47 @@
+import sys
+import math
+
+
+def get_coef(index, prompt):
+    try:
+        coef_str = sys.argv[index]
+    except:
+
+        print(prompt)
+        coef_str = input()
+    coef = float(coef_str)
+    return coef
+
+
+def get_roots(a, b, c):
+    result = []
+    D = b * b - 4 * a * c
+    if D == 0.0:
+        root = -b / (2.0 * a)
+        result.append(root)
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+        root1 = (-b + sqD) / (2.0 * a)
+        root2 = (-b - sqD) / (2.0 * a)
+        result.append(root1)
+        result.append(root2)
+    return result
+
+
+def main():
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    roots = get_roots(a, b, c)
+    len_roots = len(roots)
+    if len_roots == 0:
+        print('Нет корней')
+    elif len_roots == 1:
+        print('Один корень: {}'.format(roots[0]))
+    elif len_roots == 2:
+        print('Два корня: {} и {}'.format(roots[0], roots[1]))
+
+
+
+if __name__ == "__main__":
+    main()
